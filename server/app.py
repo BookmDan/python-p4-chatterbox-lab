7e62cdf2fa4398cb01436867d94d7e7f96b0b883
BookmDan <danoh329@gmail.com>
2023-12-10 19:20:50 -0800

    not sure

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from models import db, Message
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -14,13 +16,31 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods =['GET'])
 def messages():
-    return ''
+    messages = Message.query.all()
+    serialized_messages=[]
+    for message in messages:
+        serialized_messages = {
+            'id': message.id,
+            'body': message.body,
+            'username': message.username,
+            'created_at': message.created_at.strftime('%Y-%m-%dT%H:%M:%S'),
+        }
+        serialized_messages.append(serialized_messages)
+    return jsonify(serialized_messages)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods =['GET'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+
+    serialized_message ={
+        'id': message.id,
+        'body': message.body,
+        'username': message.username,
+        'created_at': message.created_at.strftime('%Y-%m-%dT%H:%M:%S'),
+    }
+    return jsonify(serialized_message)
 
 if __name__ == '__main__':
     app.run(port=5555)
