5b56544f1850a57b5f59eeb01bef24f0489bdd02
BookmDan <danoh329@gmail.com>
2024-01-08 10:17:58 -0800

    complete, patch delete

--- server/app.py
+++ server/app.py
@@ -16,7 +16,7 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages') #, methods =['GET']
+@app.route('/messages', methods =['GET', 'POST']) 
 def messages():
     if request.method =='GET':
         messages = Message.query.order_by('created_at').all()
@@ -26,8 +26,19 @@ def messages():
         )
         # return 'this is where messages will go'
     elif request.method == 'POST':
-        
-        return 'something else'
+        data = request.get_json()
+        # get from model Message
+        message = Message(
+            body = data['body'],
+            username = data['username']
+        )
+        # do something with message
+        db.session.add(message)
+        db.session.commit()
+        response = make_response(
+            jsonify(message.to_dict()), 201
+            # 201 http code created
+        )
     return response
     # messages = Message.query.all()
     # serialized_messages=[]
@@ -42,9 +53,25 @@ def messages():
     # return jsonify(serialized_messages)
     
 
-@app.route('/messages/<int:id>', methods =['GET'])
+@app.route('/messages/<int:id>', methods =['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return f'this is where we get message by id info {id}'
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'PATCH':
+        newMessageData = request.get_json()
+        for key in newMessageData:
+            setattr(message, key, newMessageData[key])
+        db.session.add(message)
+        db.session.commit()
+
+        response = make_response(jsonify(message.to_dict()))
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        response = make_response()
+    return response
+
+    # return f'this is where we get message by id info {id}'
     message = Message.query.get_or_404(id)
 
     serialized_message ={
